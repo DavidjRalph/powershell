81fbd13bb2d9a994894ea81243e763cc1703b515
DavidjRalph <44922970+DavidjRalph@users.noreply.github.com>
2024-02-04 19:23:49 +0000

    Add files via upload

---
+++ DateBetween.ps1
@@ -0,0 +1,6 @@
+﻿cls
+
+[datetime]$start = '2024-01-24 00:00:00'
+[datetime]$end = '2024-01-26 00:00:00'
+Get-ChildItem "C:\Users\david\OneDrive\Documents\David Powershell testing" |
+  Where-Object { $_.LastWriteTime -gt $start -and $_.LastWriteTime -lt $end | Sort-Object -Descending}
